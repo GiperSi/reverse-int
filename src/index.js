fb6cf15975cf0203156eb0a816f4b95a5a3c37dd
Dzmitry <74104111+GiperSi@users.noreply.github.com>
2021-03-15 13:11:00 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let stroka = n;
+    if (n > 0) {
+      stroka = n + '';
+      return +stroka.split('').reverse().join('');
+    } else {
+      stroka = -n + '';
+      return +stroka.split('').reverse().join('');
+    };
 }
